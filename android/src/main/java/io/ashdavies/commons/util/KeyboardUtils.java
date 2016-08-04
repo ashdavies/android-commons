7439da3cdd67cf831ee796a15bf7e116cdf552d5
Ash Davies <ash.davies@gmail.com>
2016-08-05 00:43:09 +0200

    Improve keyboard utilities

--- android/src/main/java/io/ashdavies/commons/util/KeyboardUtils.java
+++ android/src/main/java/io/ashdavies/commons/util/KeyboardUtils.java
@@ -1,5 +1,6 @@
 package io.ashdavies.commons.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -11,6 +12,35 @@ public final class KeyboardUtils {
 
   public static void dismiss(Context context, View view) {
     getManager(context).hideSoftInputFromInputMethod(view.getWindowToken(), 0);
+  }
+
+  public static void dismiss(Activity activity) {
+    dismiss(activity, activity.getCurrentFocus());
+  }
+
+  public static void dismiss(Context context, View... views) {
+    if (views == null) {
+      return;
+    }
+
+    InputMethodManager manager = getManager(context);
+    for (View view : views) {
+      if (view == null) {
+        continue;
+      }
+
+      manager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+      view.clearFocus();
+    }
+  }
+
+  public static void show(Context context, View view) {
+    if (view == null) {
+      return;
+    }
+
+    view.requestFocus();
+    getManager(context).showSoftInput(view, 0);
   }
 
   private static InputMethodManager getManager(Context context) {
