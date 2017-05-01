0cb9e18f3d1efd5402091823adc2fe6b84f94291
Ash Davies <ash.davies@gmail.com>
2017-05-01 12:43:33 +0200

    feat: extract builders module

--- android/src/main/java/io/ashdavies/commons/util/BundleUtils.java
+++ android/src/main/java/io/ashdavies/commons/util/BundleUtils.java
@@ -2,6 +2,7 @@ package io.ashdavies.commons.util;
 
 import android.os.Bundle;
 import android.os.Parcelable;
+import io.ashdavies.builders.BundleBuilder;
 import java.io.Serializable;
 
 public final class BundleUtils {
@@ -11,131 +12,54 @@ public final class BundleUtils {
   }
 
   public static Bundle create(String key, boolean value) {
-    return builder().put(key, value).build();
+    return builder().putBoolean(key, value).build();
   }
 
   public static Bundle create(String key, byte value) {
-    return builder().put(key, value).build();
+    return builder().putByte(key, value).build();
   }
 
   public static Bundle create(String key, char value) {
-    return builder().put(key, value).build();
+    return builder().putChar(key, value).build();
   }
 
   public static Bundle create(String key, short value) {
-    return builder().put(key, value).build();
+    return builder().putShort(key, value).build();
   }
 
   public static Bundle create(String key, int value) {
-    return builder().put(key, value).build();
+    return builder().putInt(key, value).build();
   }
 
   public static Bundle create(String key, long value) {
-    return builder().put(key, value).build();
+    return builder().putLong(key, value).build();
   }
 
   public static Bundle create(String key, float value) {
-    return builder().put(key, value).build();
+    return builder().putFloat(key, value).build();
   }
 
   public static Bundle create(String key, double value) {
-    return builder().put(key, value).build();
+    return builder().putDouble(key, value).build();
   }
 
   public static Bundle create(String key, String value) {
-    return builder().put(key, value).build();
+    return builder().putString(key, value).build();
   }
 
   public static Bundle create(String key, CharSequence value) {
-    return builder().put(key, value).build();
+    return builder().putCharSequence(key, value).build();
   }
 
   public static Bundle create(String key, Serializable value) {
-    return builder().put(key, value).build();
+    return builder().putSerializable(key, value).build();
   }
 
   public static Bundle create(String key, Parcelable value) {
-    return builder().put(key, value).build();
+    return builder().putParcelable(key, value).build();
   }
 
-  public static Builder builder() {
-    return new Builder();
-  }
-
-  public static class Builder {
-
-    private final Bundle bundle;
-
-    public Builder() {
-      this(new Bundle());
-    }
-
-    public Builder(Bundle bundle) {
-      this.bundle = bundle;
-    }
-
-    public Builder put(String key, boolean value) {
-      bundle.putBoolean(key, value);
-      return this;
-    }
-
-    public Builder put(String key, byte value) {
-      bundle.putByte(key, value);
-      return this;
-    }
-
-    public Builder put(String key, char value) {
-      bundle.putChar(key, value);
-      return this;
-    }
-
-    public Builder put(String key, short value) {
-      bundle.putShort(key, value);
-      return this;
-    }
-
-    public Builder put(String key, int value) {
-      bundle.putInt(key, value);
-      return this;
-    }
-
-    public Builder put(String key, long value) {
-      bundle.putLong(key, value);
-      return this;
-    }
-
-    public Builder put(String key, float value) {
-      bundle.putFloat(key, value);
-      return this;
-    }
-
-    public Builder put(String key, double value) {
-      bundle.putDouble(key, value);
-      return this;
-    }
-
-    public Builder put(String key, String value) {
-      bundle.putString(key, value);
-      return this;
-    }
-
-    public Builder put(String key, CharSequence value) {
-      bundle.putCharSequence(key, value);
-      return this;
-    }
-
-    public Builder put(String key, Serializable value) {
-      bundle.putSerializable(key, value);
-      return this;
-    }
-
-    public Builder put(String key, Parcelable value) {
-      bundle.putParcelable(key, value);
-      return this;
-    }
-
-    public Bundle build() {
-      return new Bundle(bundle);
-    }
+  public static BundleBuilder builder() {
+    return new BundleBuilder();
   }
 }
